3d3adbfd754915a4aa610a6e7988b1d63db0765d
Daksh rana <ranadaksh205@gmail.com>
2025-04-18 00:48:20 +0530

    made changes 2

--- client/src/pages/common/Home/index.js
+++ client/src/pages/common/Home/index.js
@@ -32,23 +32,24 @@ function Home() {
 
   return (
     user && (
-      <div>
+      <div className="p-4">
         <PageTitle title={`Hi ${user.name}, Welcome to Quizzy`} />
         <div className="divider"></div>
         <Row gutter={[16, 16]}>
           {exams.map((exam) => (
-            <Col span={6}>
-              <div className="card-lg flex flex-col gap-1 p-2">
-                <h1 className="text-2xl">{exam?.name}</h1>
+            <Col xs={24} sm={24} md={12} lg={8} xl={6} key={exam._id}>
+              <div className="card-lg flex flex-col gap-1 p-4 h-full">
+                <h1 className="text-xl md:text-2xl mb-2">{exam?.name}</h1>
 
-                <h1 className="text-md">Category : {exam.category}</h1>
-
-                <h1 className="text-md">Total Marks : {exam.totalMarks}</h1>
-                <h1 className="text-md">Passing Marks : {exam.passingMarks}</h1>
-                <h1 className="text-md">Duration : {exam.duration}</h1>
+                <div className="flex-grow">
+                  <h1 className="text-sm md:text-md mb-1">Category : {exam.category}</h1>
+                  <h1 className="text-sm md:text-md mb-1">Total Marks : {exam.totalMarks}</h1>
+                  <h1 className="text-sm md:text-md mb-1">Passing Marks : {exam.passingMarks}</h1>
+                  <h1 className="text-sm md:text-md mb-3">Duration : {exam.duration}</h1>
+                </div>
 
                 <button
-                  className="primary-outlined-btn"
+                  className="primary-outlined-btn w-full mt-auto"
                   onClick={() => navigate(`/user/write-exam/${exam._id}`)}
                 >
                   Start Exam
